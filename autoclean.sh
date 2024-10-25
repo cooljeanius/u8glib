0bd1d1fc0af9e400e766906fa544dc66f81ec453
Eric Gallager <egall@gwmail.gwu.edu>
2024-10-25 00:50:47 -0400

    autoreconf for 2024

--- autoclean.sh
+++ autoclean.sh
@@ -3,18 +3,20 @@
 set -ex
 
 for auxfile in $0 autogen.sh config.guess config.rpath config.status config.sub configure depcomp install-sh missing; do
-  if test -e ${auxfile}; then chmod 755 ${auxfile}; fi
+  if test -e "${auxfile}"; then chmod 755 "${auxfile}"; fi
 done
 
-if test -e Makefile; then make clean; fi
-if test -e Makefile; then make distclean; fi
+if test -e Makefile; then make clean; else echo "clean: Makefile is gone"; fi
+if test -e Makefile; then make distclean; else echo "dc: Makefile is gone"; fi
 
-rm -f *.o
+rm -f ./*.o ./*~
 
+rm -f autoscan.log || ls ./*.log
 rm -f config.log
 rm -f config.status
 rm -f config.h
 rm -f Makefile
 rm -rf ./autom4te.cache/ || rmdir autom4te.cache
 rm -f gmon.out
+rm -rf ./*.dSYM || rmdir ./*.dSYM
 
--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,14 @@
 set -ex
 
 PROGNAME="$0"
-DN_EXEC=`dirname "${PROGNAME}"`
+DN_EXEC=$(dirname "${PROGNAME}")
 if [ ! "${DN_EXEC}" = "" ]; then
     DN_EXEC="${DN_EXEC}/"
 else
     DN_EXEC="./"
 fi
 
+# shellcheck disable=SC2086
 ${DN_EXEC}autoclean.sh
 
 rm -f configure
@@ -22,6 +23,7 @@ rm -f install-sh
 rm -f missing
 rm -f depcomp
 
+# shellcheck disable=SC2050
 if [ 0 = 1 ]; then
 autoscan
 else
@@ -29,30 +31,31 @@ else
 touch NEWS
 touch README
 touch AUTHORS
-if test -z "$(grep $(whoami) AUTHORS)"; then echo "`whoami`" >> AUTHORS; fi
+if test -z "$(grep "$(whoami)" AUTHORS)"; then whoami >> AUTHORS; fi
 touch THANKS
-if test -z "$(grep $(whoami) THANKS)"; then
-  echo "Thanks, `whoami`" >> THANKS;
+if test -z "$(grep "$(whoami)" THANKS)"; then
+  echo "Thanks, $(whoami)" >> THANKS;
 fi
 touch ChangeLog
 touch config.h.in
 
 LIBTOOLIZE=false
-if test -x "`which libtoolize`"; then
+if test -x "$(which libtoolize)"; then
   LIBTOOLIZE=libtoolize
-elif test -x "`which glibtoolize`"; then
+elif test -x "$(which glibtoolize)"; then
   LIBTOOLIZE=glibtoolize
 fi
 ${LIBTOOLIZE} --force --copy --install --automake
-aclocal
-automake --copy --add-missing --gnits
-autoconf
+aclocal -I m4
+automake --copy --add-missing --gnits -Wno-portability
+autoconf -Wno-obsolete
 
-autoreconf -fvi -Wall
+autoreconf -fvi -Wall -Wno-obsolete
 if [ $? -eq 2 ]; then
   autoreconf # run twice to get rid of 'ltmain.sh not found'
 fi
 
+# shellcheck disable=SC2050
 if [ 1 = 1 ]; then
 rm -rf build
 mkdir -p build
